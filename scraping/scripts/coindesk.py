d65041145d07a74086b69432f295d8eeed6c03d1
Darius Oh <ohsterx@gmail.com>
2021-01-31 17:35:57 +0800

    Fix coindesk & bitnewstoday scrappers

---
+++ scraping/scripts/coindesk.py
@@ -0,0 +1,72 @@
+import pandas as pd
+import requests
+from datetime import datetime, timedelta
+import time
+
+def coindesk_scrape(entity, start_date, end_date):
+
+    # dictionary to store the relevant data
+    data_store = {'date_time':[], 'title':[], 'excerpt':[], 'article_url':[],  'author':[], 'image_url':[]}
+
+    # request and get url
+    def retrieve_data(entity, page):
+        # link to retrieve data from
+        url = 'https://www.coindesk.com/wp-json/v1/search?keyword=' + str(entity) + '&page=' + str(page)
+        data = requests.get(url).json()
+        return data['results']
+
+    page = 1
+    page_data = retrieve_data(entity, page)
+
+    # retrieve datetime for the last submission in the page
+    last = end_date
+
+    while last >= start_date:
+        # if there are no search results, stop
+        if page_data == []:
+            break
+        else:
+            for article in page_data:
+                #add 8 hours to convert to SGT
+                date_time =  datetime.strptime(article['date'], "%Y-%m-%dT%H:%M:%S") + timedelta(hours=8)
+                last = date_time
+
+                # retrieve relevant information from article
+                if date_time <= end_date and date_time >= start_date: 
+                    data_store['date_time'].append(date_time)
+
+                    title = article['title']
+                    data_store['title'].append(title)
+                
+                    excerpt = article['text']
+                    data_store['excerpt'].append(excerpt)
+
+                    article_url = article['url']
+                    article_url = article_url.replace("\\", "")
+                    data_store['article_url'].append(article_url)
+
+                    author = article['author'][0]['name']
+                    data_store['author'].append(author)
+
+                    # try except block for articles without image url
+                    try:
+                        image = article['images']['images']['desktop']['src']
+                        data_store['image_url'].append(image)
+                    except:
+                        data_store['image_url'].append('')
+            
+            # increment page number
+            page += 1
+            page_data = retrieve_data(entity, page)
+
+    df = pd.DataFrame(data_store)
+    return df
+
+
+############### Testing ################
+##entity = 'ethereum'
+##start_date = datetime(2021, 1, 1)
+##end_date = datetime(2021, 1, 31,23,59,59)
+##df = coindesk_scrape(entity, start_date, end_date)
+##df.to_csv("../data/coindesk_feed/010121_to_310121.csv")
+# ######################################
