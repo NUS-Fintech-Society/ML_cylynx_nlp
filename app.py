ed7d4f242b6fc8c3c07a7a6b698809353b3e9d76
reoneo97 <reoneo97@gmail.com>
2021-06-03 12:21:22 +0000

    added utility functions for data cleaning

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
 import streamlit as st
+from prediction import predict
+
+import pandas as pd
+import json
+from tqdm import tqdm
+
+df = pd.read_csv("data/data.csv")
+df["text"] = df["title"] + " " + df["excerpt"].fillna("")
+inp = df["text"].tolist()
+output = []
+
+for i in tqdm(range(len(inp)//64)):
+    in_slice = inp[i*64:(i+1)*64]
+    out = predict(in_slice)
+    output.extend(out)
+with open("output.jsonl","w+",encoding="utf-8") as f:
+    json.dump(output,f)
